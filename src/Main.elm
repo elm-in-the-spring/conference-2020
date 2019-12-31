600db20b618eef3e9fa8f1cd1a40c9a274cdb2dc
Sade Smith <smithsade13@gmail.com>
2019-12-31 17:51:56 -0600

    style standalone page

--- src/Main.elm
+++ src/Main.elm
@@ -203,8 +203,8 @@ newsletterContent =
 
 divider : Html Msg
 divider =
-
     div [class "divider--hatched"] []
+
 footerContent : Html Msg
 footerContent =
     footer [] [ div [class "footer__content columns"] [
@@ -217,35 +217,45 @@ footerContent =
     ]
 
     ]
+
+standAlonePageHeader : Html Msg
+standAlonePageHeader =
+    div [ class "hero" ]
+        [ div [class "logo-container"]
+            [ a [href "#"] [img [src "%PUBLIC_URL%/images/hero-logo.png", alt "Elm In the Spring", class "logo"] []]]
+        , navigationContent ]
+
 codeOfConduct : Html Msg
 codeOfConduct =
-    main_ [ id "code-of-conduct" ]
-        [ h1 [ id "conference-code-of-conduct" ] [ text "Conference Code of Conduct" ]
-        , p []
-            [ a [ onClick (NavigateTo "/"), href "/" ] [ text "back" ]
-            ]
-        , p [] [ text "All attendees, speakers, sponsors and volunteers at our conference are required to agree with the following code of conduct. Organisers will enforce this code throughout the event. We expect cooperation from all participants to help ensure a safe environment for everybody." ]
-        , h2 [ id "need-help" ] [ text "Need Help?" ]
-        , p []
-            [ text "In advance of the conference, please contact the organizing team at "
-            , a [ href "mailto:hello@elminthespring.org" ] [ text "hello@elminthespring.org" ]
-            , text " any time you need help or have questions or concerns. In the weeks before the conference, we’ll send out direct contact info for the organizers so you can reach out to us in real time via text, phone, or Twitter."
-            ]
-        , h2 [ id "the-quick-version" ] [ text "The Quick Version" ]
-        , p [] [ text "Our conference is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks, workshops, parties, Twitter and other online media. Conference participants violating these rules may be sanctioned or expelled from the conference without a refund at the discretion of the conference organisers." ]
-        , h2 [ id "the-less-quick-version" ] [ text "The Less Quick Version" ]
-        , p [] [ text "Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion, technology choices, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention. " ]
-        , p [] [ text "Participants asked to stop any harassing behavior are expected to comply immediately. " ]
-        , p [] [ text "Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not use sexualised images, activities, or other material. Booth staff (including volunteers) should not use sexualised clothing/uniforms/costumes, or otherwise create a sexualised environment. " ]
-        , p [] [ text "If a participant engages in harassing behavior, the conference organisers may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund. " ]
-        , p []
-            [ text "If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of conference staff immediately." ]
-        , p [] [ text " Conference staff can be identified as they&#39;ll be wearing branded clothing and/or badges. " ]
-        , p [] [ text "Conference staff will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the conference. We value your attendance. " ]
-        , p [] [ text "We expect participants to follow these rules at conference and workshop venues and conference-related social events." ]
-        , p []
-            [ a [ onClick (NavigateTo "/"), href "/" ] [ text "back" ]
-            ]
+    main_ [ class "page--stand-alone" ]
+        [
+        standAlonePageHeader
+        , divider
+        , div [class "container container--wrapper"] [
+            h1 [ id "conference-code-of-conduct" ] [ text "Conference Code of Conduct" ]
+                     , p [] [ text "All attendees, speakers, sponsors and volunteers at our conference are required to agree with the following code of conduct. Organisers will enforce this code throughout the event. We expect cooperation from all participants to help ensure a safe environment for everybody." ]
+                    , h2 [ id "need-help" ] [ text "Need Help?" ]
+                    , p []
+                        [ text "In advance of the conference, please contact the organizing team at "
+                        , a [ href "mailto:hello@elminthespring.org", class "animate black" ] [ text "hello@elminthespring.org" ]
+                        , text " any time you need help or have questions or concerns. In the weeks before the conference, we’ll send out direct contact info for the organizers so you can reach out to us in real time via text, phone, or Twitter."
+                        ]
+                    , h2 [ id "the-quick-version" ] [ text "The Quick Version" ]
+                    , p [] [ text "Our conference is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks, workshops, parties, Twitter and other online media. Conference participants violating these rules may be sanctioned or expelled from the conference without a refund at the discretion of the conference organisers." ]
+                    , h2 [ id "the-less-quick-version" ] [ text "The Less Quick Version" ]
+                    , p [] [ text "Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion, technology choices, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention. " ]
+                    , p [] [ text "Participants asked to stop any harassing behavior are expected to comply immediately. " ]
+                    , p [] [ text "Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not use sexualised images, activities, or other material. Booth staff (including volunteers) should not use sexualised clothing/uniforms/costumes, or otherwise create a sexualised environment. " ]
+                    , p [] [ text "If a participant engages in harassing behavior, the conference organisers may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund. " ]
+                    , p []
+                        [ text "If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of conference staff immediately." ]
+                    , p [] [ text " Conference staff can be identified as they'll be wearing branded clothing and/or badges. " ]
+                    , p [] [ text "Conference staff will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the conference. We value your attendance. " ]
+                    , p [] [ text "We expect participants to follow these rules at conference and workshop venues and conference-related social events." ]
+
+        ]
+        , divider
+        , footerContent
         ]
 
 
